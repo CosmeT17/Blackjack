3297d965ec305872546d25ca37ecd1e8ce342563
Cosme Manuel Tejeda <cosme.m.tejeda@gmail.com>
2022-08-04 21:14:45 -0500

    Update ascii.py

--- ascii.py
+++ ascii.py
@@ -1,3 +1,8 @@
+# TESTING -------------------------------------------------------------------------
+# import random
+# from replit import clear
+# from readchar import readkey
+# ---------------------------------------------------------------------------------
 digits = [
     [
         "  __  ",
@@ -78,28 +83,41 @@ def get_number(num, smush = False):
                 l_gap = len(l_digit[line]) - len(l_digit[line].rstrip(' '))
                 r_gap = len(r_digit[line]) - len(r_digit[line].lstrip(' '))
                 gaps.append(l_gap + r_gap)
-            gap = min(gaps) + 1
+            gap = min(gaps)
+            if smush: gap += 1
         else: gap = -1
-
+        
         for line_num in range(len(l_digit)):
             line = list(l_digit[line_num])
 
             for _ in range(gap):
                 if line[-1] == ' ': line.pop()
                 else:
-                    l_char = line[-1]
-                    r_char = r_digit[line_num][0]
-                    
-                    if r_char != ' ' and l_char != ')': line[-1] = r_char
-                    ascii[pos + 1][line_num] = ascii[pos + 1][line_num][1:]
+                    if smush:
+                        l_char = line[-1]
+                        r_char = r_digit[line_num][0]
+                        
+                        if r_char != ' ' and l_char != ')': line[-1] = r_char
+                            
+                    r_digit[line_num] =r_digit[line_num][1:]
             txt[line_num] += ''.join(line)
     return txt
 
-def prt(x): 
-    for i in x: print(i)
+# ---------------------------------------------------------------------------------
+# TESTING get_number():
+
+# def prt(x): 
+#     for i in x: print(i)
 
 # for i in range(10):
 #     for j in range(10):
 #         clear()
 #         prt(get_number(f"{i}{j}"))
+#         prt(get_number(f"{i}{j}", True))
 #         readkey()
+
+# num = random.randrange(1000000000000)
+# clear()
+# prt(get_number(num))
+# prt(get_number(num, True))
+# ---------------------------------------------------------------------------------
